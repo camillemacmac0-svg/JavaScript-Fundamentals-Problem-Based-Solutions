0fdc34f5d03d4f21fd49ce36b3ba836c30426d71
camillemacmac0-svg <camillemacmac0@gmail.com>
2026-05-09 12:55:59 +0800

    Add shuffle functionality to card display

--- script.js
+++ script.js
@@ -361,4 +361,20 @@ nextBtn.addEventListener('click', () => {
         showCard();
     }
 });
+
+shuffleBtn.addEventListener('click', () => {
+
+    for (let i = cards.length - 1; i > 0; i--) {
+
+        const j =
+            Math.floor(Math.random() * (i + 1));
+
+        [cards[i], cards[j]] =
+            [cards[j], cards[i]];
+    }
+
+    currentIndex = 0;
+
+    showCard();
+});
 document.addEventListener('DOMContentLoaded', loadData);
